6794de8ad410fbfbe6f65ce0dfd623e787b3dd2b
Krithik Kumar <krithikkumar961@gmail.com>
2026-05-08 21:56:15 +0530

    CHANGING LOGIN FORMAT

--- config/db.js
+++ config/db.js
@@ -1,13 +1,26 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`  MongoDB connected: ${conn.connection.host}`);
+
+    const conn = await mongoose.connect(
+      process.env.MONGO_URI,
+      {
+        dbName: 'homefeast'
+      }
+    );
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+    console.log(`Database Name: ${conn.connection.name}`);
+
   } catch (error) {
-    console.error(`  MongoDB error: ${error.message}`);
+
+    console.error(`MongoDB error: ${error.message}`);
     process.exit(1);
+
   }
+
 };
 
 module.exports = connectDB;
